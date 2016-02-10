b067d2f9eac8b9baede72ecb2426929a3add612d
Juan Santiago Acevedo <js.acevedo10@uniandes.edu.co>
2016-02-10 08:54:59 -0500

    Server & Packages Created.

---
+++ src/main/java/com/arquisoft/SATT/Main.java
@@ -0,0 +1,24 @@
+
+package com.arquisoft.SATT;
+
+import com.sun.grizzly.http.SelectorThread;
+import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		final String baseUri = "http://localhost:"+(System.getenv("PORT")!=null?System.getenv("PORT"):"9998")+"/";
+        final Map<String, String> initParams = new HashMap<String, String>();
+
+        initParams.put("com.sun.jersey.config.property.packages","com.arquisoft.SATT.recursos");
+        
+        System.out.println("Starting grizzly...");
+        SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
+        System.out.println(String.format("Jersey started with WADL available at %sapplication.wadl.",baseUri, baseUri));
+        System.out.println("Press enter to stop server...");
+        System.in.read();
+        threadSelector.stopEndpoint();
+	}
+}
