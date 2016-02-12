f45c6a56d10bf32264623ea62191c360421351b8
Daniel Soto <d.soto11@uniandes.edu.co>
2016-02-12 17:48:24 -0500

    Utilidades para MONGO

---
+++ src/main/java/com/arquisoft/SATT/utilidades/KeyValueSearch.java
@@ -0,0 +1,41 @@
+package com.arquisoft.SATT.utilidades;
+
+/**
+ * Clase para empaquetar las parejas de valores de JSON para una búsqueda en Mongo
+ * @author danielsoto
+ *
+ */
+public class KeyValueSearch{
+
+	public enum SearchType{
+		EQUALS, GREATER, LESS, ID;
+	}
+	
+	private String key;
+	private Object value;
+	private SearchType type;
+	
+	/**
+	 * Crea un objeto que representa una entrada JSON de busqueda.
+	 * @param key - La llave de la columna que se desea filtrar
+	 * @param value - El valor de comparacion para el filtro
+	 * @param type - El tipo de comparacion del filtro, definida por el enum SearchType.
+	 */
+	public KeyValueSearch(String key, Object value, SearchType type){
+		this.key = key;
+		this.value = value;
+		this.type = type;
+	}
+	
+	public String getKey(){
+		return key;
+	}
+	
+	public Object getValue(){
+		return value;
+	}
+	
+	public SearchType getType(){
+		return type;
+	}
+}
