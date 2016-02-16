b7f1af3a1df68dcfd3669d8f674f7afdf715ae21
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2016-02-16 01:19:31 -0500

    Remove unused method

--- src/main/java/com/arquisoft/SATT/utilidades/GeoAsistant.java
+++ src/main/java/com/arquisoft/SATT/utilidades/GeoAsistant.java
@@ -1,7 +1,5 @@
 package com.arquisoft.SATT.utilidades;
 
-import com.arquisoft.SATT.mundo.Zona;
-
 public class GeoAsistant {
 
 	public static int R = 6371000; 
@@ -20,9 +18,5 @@ public class GeoAsistant {
 		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
 		return R * c;
-	}
-	
-	public static Zona getLocationZona(double lat, double lng) {
-		return Zona.Antioquia;
 	}
 }
