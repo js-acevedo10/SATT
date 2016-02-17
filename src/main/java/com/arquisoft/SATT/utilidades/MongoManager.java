58e7ec3641af6a04e3bcce23f21ede4568a19966
js-acevedo10 <js.acevedo10@uniandes.edu.co>
2016-02-16 22:13:00 -0500

    Pool from 10 to 1000 Mongo

--- src/main/java/com/arquisoft/SATT/utilidades/MongoManager.java
+++ src/main/java/com/arquisoft/SATT/utilidades/MongoManager.java
@@ -6,7 +6,9 @@ import org.bson.Document;
 import org.bson.types.ObjectId;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
+import com.mongodb.MongoOptions;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.DeleteResult;
@@ -28,8 +30,11 @@ public class MongoManager {
 	 * @return La base de datos en Mongo para buscar.
 	 */
 	private static MongoDatabase initMongoDB(){
-		if (mongo==null || URI == null || db == null){ 
-			URI  = new MongoClientURI(DBURI); 
+		if (mongo==null || URI == null || db == null){
+			MongoClientOptions options = MongoClientOptions.builder()
+				    .connectionsPerHost(1000)
+				    .build();
+			URI  = new MongoClientURI(DBURI, new MongoClientOptions.Builder(options)); 
 			mongo = new MongoClient(URI);
 			db = mongo.getDatabase(URI.getDatabase());
 		}
