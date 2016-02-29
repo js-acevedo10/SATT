5a9a8ec68bd21692cea22709d898780e1b569da2
Juan Santiago Acevedo <js.acevedo10@uniandes.edu.co>
2016-02-29 15:10:19 -0500

    ERROR

--- src/main/java/com/arquisoft/SATT/Main.java
+++ src/main/java/com/arquisoft/SATT/Main.java
@@ -1,5 +1,7 @@
-
 package com.arquisoft.SATT;
+
+
+import org.glassfish.grizzly.http.server.HttpServer;
 
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
@@ -9,8 +11,6 @@ import java.io.IOException;
 import java.net.URI;
 
 import javax.ws.rs.core.UriBuilder;
-
-import org.glassfish.grizzly.http.server.HttpServer;
 
 public class Main {
 	private static URI getBaseURI(String hostname, int port) {
