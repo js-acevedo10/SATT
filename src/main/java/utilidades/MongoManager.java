a9f37ccc5fbad00200b11f3dc143cce3a532ffef
Daniel Soto <d.soto11@uniandes.edu.co>
2016-03-15 14:51:51 -0500

    back

--- src/main/java/utilidades/MongoManager.java
+++ src/main/java/utilidades/MongoManager.java
@@ -30,7 +30,7 @@ public class MongoManager {
 	 */
 	private static MongoDatabase initMongoDB(){
 		if (mongo==null || URI == null || db == null){ 
-			URI  = new MongoClientURI(DBURI, MongoClientOptions.builder().minConnectionsPerHost(0).connectionsPerHost(5)); 
+			URI  = new MongoClientURI(DBURI, MongoClientOptions.builder().minConnectionsPerHost(2).connectionsPerHost(5)); 
 			mongo = new MongoClient(URI);
 			db = mongo.getDatabase(URI.getDatabase());
 		}
