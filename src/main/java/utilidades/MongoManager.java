408967e83602f5922e73eef2be07e13c60cb2a07
Daniel Soto <d.soto11@uniandes.edu.co>
2016-03-15 00:36:50 -0500

    No tan crack

--- src/main/java/utilidades/MongoManager.java
+++ src/main/java/utilidades/MongoManager.java
@@ -30,7 +30,7 @@ public class MongoManager {
 	 */
 	private static MongoDatabase initMongoDB(){
 		if (mongo==null || URI == null || db == null){ 
-			URI  = new MongoClientURI(DBURI, MongoClientOptions.builder().minConnectionsPerHost(4000)); 
+			URI  = new MongoClientURI(DBURI, MongoClientOptions.builder().minConnectionsPerHost(100)); 
 			mongo = new MongoClient(URI);
 			db = mongo.getDatabase(URI.getDatabase());
 		}
