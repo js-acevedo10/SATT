75db6e34246b6fb95f32440333ba2cf2105db268
Daniel Soto <d.soto11@uniandes.edu.co>
2016-03-15 14:33:08 -0500

    only 10 fakers

--- src/main/java/utilidades/MongoManager.java
+++ src/main/java/utilidades/MongoManager.java
@@ -30,7 +30,7 @@ public class MongoManager {
 	 */
 	private static MongoDatabase initMongoDB(){
 		if (mongo==null || URI == null || db == null){ 
-			URI  = new MongoClientURI(DBURI, MongoClientOptions.builder().minConnectionsPerHost(5).connectionsPerHost(20)); 
+			URI  = new MongoClientURI(DBURI, MongoClientOptions.builder().minConnectionsPerHost(10).connectionsPerHost(10)); 
 			mongo = new MongoClient(URI);
 			db = mongo.getDatabase(URI.getDatabase());
 		}
