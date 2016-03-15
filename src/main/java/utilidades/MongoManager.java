e909595ee0c00750d6ea199e0fa7071b60e94556
Daniel Soto <d.soto11@uniandes.edu.co>
2016-03-15 14:13:18 -0500

    dont prepare

--- src/main/java/utilidades/MongoManager.java
+++ src/main/java/utilidades/MongoManager.java
@@ -30,7 +30,7 @@ public class MongoManager {
 	 */
 	private static MongoDatabase initMongoDB(){
 		if (mongo==null || URI == null || db == null){ 
-			URI  = new MongoClientURI(DBURI, MongoClientOptions.builder().minConnectionsPerHost(20).connectionsPerHost(20)); 
+			URI  = new MongoClientURI(DBURI, MongoClientOptions.builder().minConnectionsPerHost(0).connectionsPerHost(20)); 
 			mongo = new MongoClient(URI);
 			db = mongo.getDatabase(URI.getDatabase());
 		}
